3f2e90ec0c6a09f05f0c0b814676d6403be2b83c
Nadasoliman29 <nadaezzat87147@gmail.com>
2025-09-23 00:42:17 +0300

    new

--- src/Services/INTERFACES.ts
+++ src/Services/INTERFACES.ts
@@ -50,14 +50,3 @@ export interface RoomsListInterface {
   discount: number;
   images: [string]
 }
-export interface UserListInterface {
-  _id: string;
-  userName: string;
-  profileImage: string
-  email: string;
-  role: string;
-  phoneNumber: number;
-  country:string;
-
-}
-

edf865fbdd797a7783dda98ef894595a34197455
ftmamhmod <ftmamhmod2@gmail.com>
2025-09-25 11:02:20 +0300

    feat: add Home component for landing page with booking functionality

--- src/Modules/Shared/MasterLayout/MasterLayout.tsx
+++ src/Modules/Shared/MasterLayout/MasterLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import MasterNavbar from "../MasterNavbar/MasterNavbar";
 import MasterFooter from "../MasterFooter/MasterFooter";
+import { Box } from "@mui/material";
 
 export default function MasterLayout() {
   return (
     <>
       <MasterNavbar />
+      <Box px={{ xs: 2, sm: 4, md: 8, lg: 20 }} py={6}>
+        <Outlet />
+      </Box>
 
-      <Outlet />
       <MasterFooter />
     </>
   );
